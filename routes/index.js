b7611779ea004c5c93834da645395b9479e14086
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-10-19 18:14:46 +0200

    Add form, save messages

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,36 @@
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
+    text: 'Hi there!',
+    user: 'Amando',
     added: new Date(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
+    text: 'Hello World!',
+    user: 'Charles',
     added: new Date(),
   },
 ];
 
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res) => {
-  res.render("index", { title: "Mini Messageboard", messages });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body['author-name'],
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
